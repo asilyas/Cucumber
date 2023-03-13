4e7985efb7b9b462110ff1e4ed0335e0e4f7c537
asilyas <ilysuckc@gmail.com>
2023-03-13 19:51:25 +0300

    last commit

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -38,7 +39,9 @@ public class Driver {
                     break;
                 case "chrome" :
                     WebDriverManager.chromedriver().setup();
-                    driver=new ChromeDriver();
+                    ChromeOptions options=new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
+                    driver = new ChromeDriver(options);
                 case "safari" :
                     WebDriverManager.safaridriver().setup();
                     driver=new SafariDriver();
@@ -48,11 +51,10 @@ public class Driver {
                     driver=new EdgeDriver();
 
             }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         }
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         return driver;
 
